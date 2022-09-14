aeaeefa03a1353783ddd6c7e7e91bdee4d0e8128
MrProstos <vladmihin28@gmail.com>
2022-09-14 21:15:10 +0300

    .

---
+++ database/migrations/2022_09_14_175308_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('title');
+            $table->string('link');
+            $table->string('description');
+            $table->string('category');
+            $table->Date('pubDate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};

0db1211ce83c0184c54fcf1446f29d8d2dc68562
MrProstos <vladmihin28@gmail.com>
2022-09-21 19:53:01 +0300

    .

--- app/Models/MemcachedServer.php
+++ app/Models/MemcachedServer.php
@@ -2,70 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Http\Request;
-use Memcached;
+use Memcache;
 
 /**
  *  A class for working with the memcached server
  *
  */
-class MemcachedServer
+class MemcachedServer extends Memcache
 {
     private const HOST = 'localhost';
     private const PORT = 11211;
-    private static ?Memcached $instance = null;
 
     public function __construct()
     {
-        if (self::$instance === null) {
-            self::$instance = new Memcached();
-            self::$instance->addServer(self::HOST, self::PORT);
-        }
-        return self::$instance;
-    }
-
-    /**
-     * @return Memcached|null
-     */
-    public function getMemcached(): ?Memcached
-    {
-        return self::$instance;
-    }
-
-    /**
-     * @param Request $request
-     * @return string
-     */
-    public function setKey(Request $request): string
-    {
-        return $request->route('creator') . '/' . implode('/', $request->toArray());
-    }
-
-    /**
-     * Get value by key
-     *
-     * @param string $key
-     * @return mixed
-     */
-    public function get(string $key): mixed
-    {
-        $value = $this->getMemcached()->get($key);
-        if ($this->getMemcached()->getResultCode() === Memcached::RES_NOTFOUND) {
-            return null;
-        }
-        return $value;
-    }
-
-    /**
-     * Store an item
-     *
-     * @param string $key
-     * @param mixed $value
-     * @param int $ttl
-     * @return bool
-     */
-    public function set(string $key, mixed $value, int $ttl): bool
-    {
-        return $this->getMemcached()->set($key, $value, $ttl);
+        parent::addServer(self::HOST, self::PORT);
     }
 }
--- tests/Models/MemcachedServerTest.php
+++ tests/Models/MemcachedServerTest.php
@@ -3,14 +3,17 @@
 namespace Tests\Models;
 
 use App\Models\MemcachedServer;
-use PHPUnit\Framework\TestCase;
+use App\Models\News;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Tests\TestCase;
 
 class MemcachedServerTest extends TestCase
 {
     public function testConstruct()
     {
         $memcached = new MemcachedServer();
-        $memcached->getMemcached()->set('test', 'test');
-        $this->assertEquals('test', $memcached->getMemcached()->get('test'));
+        $memcached->set('test', 'test');
+        $this->assertEquals('test', $memcached->get('test'));
+        $memcached->delete('test');
     }
 }
